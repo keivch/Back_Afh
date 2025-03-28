0f3c94f96fc3246a99b7077e2a31aafa67ce574b
Sebastian Hernadez <sebastian.hernandez.scarpetta@correounivalle.edu.co>
2025-03-28 15:49:29 -0500

    creacion de los modelos finalizada

--- afh_api/users/models.py
+++ afh_api/users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Users(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    type = models.CharField(max_length=500, null=True)
+    role = models.CharField(max_length=500, null=True)
 
     def __str__(self):
         return self.user.username
