0404021e09e27cb07e64aaa7c2603a42855ddd35
Sebastian Hernadez <sebastian.hernandez.scarpetta@correounivalle.edu.co>
2025-04-06 12:16:02 -0500

    correcciones y cambios en el serializador, todo testeado y funciona correctamente

--- afh_api/users/Serializers.py
+++ afh_api/users/Serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Users
+from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+class UserSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Users
         fields = '__all__'
