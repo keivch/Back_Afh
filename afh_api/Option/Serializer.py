5a0de507a65a0d2efd97a2a3140b8c74ce4f75ca
Sebastian Hernadez <sebastian.hernandez.scarpetta@correounivalle.edu.co>
2025-06-29 12:03:26 -0500

    Remove locale setting from serializers to improve portability

--- afh_api/Option/Serializer.py
+++ afh_api/Option/Serializer.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 from .models import Option
 from item.Serializer import ItemSerializer
-import locale
 
-locale.setlocale(locale.LC_ALL, 'es_CO.UTF-8')
 
 class OptionSerializer(serializers.ModelSerializer):
     items = ItemSerializer(many=True, read_only=True)
