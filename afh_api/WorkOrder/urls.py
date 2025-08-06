77e6d0710abc74d85896aeb9db6c289cb5d4a358
keivch <sebastian.hernandezz.scarpetta@correounivalle.edu.co>
2025-08-06 13:59:45 -0500

    fix: eliminando rutas inseguras

--- afh_api/WorkOrder/urls.py
+++ afh_api/WorkOrder/urls.py
@@ -5,7 +5,6 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register('WorkOrder', views.WorkOrderViewSet, basename='CrudWorkOrder')
 urlpatterns = [
-    path('', include(router.urls)),
     path('workorders/', views.get_work_orders_view, name='get-work-orders'),
     path('workorder/<int:id>/', views.get_work_order_by_id_view, name='get-work-order-by-id'),
     path('pdf/<int:id_workorder>/', views.pdf_quote_view, name='pdf-quote-view'),
